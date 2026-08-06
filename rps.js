286adb404e445690c4ba6a1092c46e8fcab88715
Jeevan s <sathishjeevan2004@gmail.com>
2026-08-06 23:02:59 +0530

    infinite setup

--- rps.js
+++ rps.js
@@ -1,22 +1,26 @@
-// const user = prompt("enter Rock Paper Scissor:").trim().toLowerCase();
-const user = "rock";
-console.log(user);
-const options = ["paper", "rock", "scissor"];
-const bot = options[Math.floor(Math.random() * options.length)];
-console.log(bot);
-if (user === bot) {
-  console.log("draw");
-} else if (
-  (user === "rock" && bot === "paper") |
-  (user === "paper" && bot === "scissor") |
-  (user === "scissor" && bot === "rock")
-) {
-  console.log("bot win");
-} else if (
-  user === "paper" &&
-  (bot === "rock") | (user === "scissor") &&
-  (bot === "paper") | (user === "rock") &&
-  bot === "scissor"
-) {
-  console.log("user win");
+var state = true;
+
+while (state) {
+  const user = prompt("Enter Rock Paper Scissor:").trim().toLowerCase();
+
+  const options = ["paper", "rock", "scissor"];
+  const bot = options[Math.floor(Math.random() * options.length)];
+
+  console.log("Bot:", bot);
+  if (user === bot) {
+    console.log("Draw");
+  }
+  if (user === "quit") {
+    state = false;
+  } else if (!options.includes(user)) {
+    console.log("invalid input");
+  } else if (
+    (user === "rock" && bot === "paper") ||
+    (user === "paper" && bot === "scissor") ||
+    (user === "scissor" && bot === "rock")
+  ) {
+    console.log("Bot Wins");
+  } else {
+    console.log("user win");
+  }
 }

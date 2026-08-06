b77ae37b925d6a4a116d7af2da382fb04c528878
Jeevan s <sathishjeevan2004@gmail.com>
2026-08-06 23:30:02 +0530

    advance rps

--- rps.js
+++ rps.js
@@ -1,7 +1,28 @@
 var state = true;
+var userScore =0;
+var botScore =0;
+let freq ={}
+let ikey =
+
+
 
 while (state) {
+    console.log(freq)
   const user = prompt("Enter Rock Paper Scissor:").trim().toLowerCase();
+  if(freq[user]){
+      
+      freq[user]++
+      
+  }else{
+      freq[user]=1
+      
+  }
+  for( var keys in freq){
+      console.log(freq[keys])
+  }
+ 
+  
+  
 
   const options = ["paper", "rock", "scissor"];
   const bot = options[Math.floor(Math.random() * options.length)];
@@ -9,9 +30,20 @@ while (state) {
   console.log("Bot:", bot);
   if (user === bot) {
     console.log("Draw");
+    
   }
   if (user === "quit") {
-    state = false;
+      console.log(`Bot-Score:${botScore}| user-score:${userScore}`)
+      if(botScore>userScore){
+          console.log("bot is winner")
+          state=false
+      }
+      else{
+          console.log("user is winner")
+          state=false
+      }
+    
+    
   } else if (!options.includes(user)) {
     console.log("invalid input");
   } else if (
@@ -20,7 +52,9 @@ while (state) {
     (user === "scissor" && bot === "rock")
   ) {
     console.log("Bot Wins");
-  } else {
+    botScore+=1
+  } else  {
     console.log("user win");
+    userScore+=1
   }
 }

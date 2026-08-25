1c308cd32c6939a94ea394c1b2cd231d43bca51f
Jeevan s <sathishjeevan2004@gmail.com>
2026-08-25 10:28:39 +0530

    getting location from user using geolocation

--- backend/server1/index.js
+++ backend/server1/index.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { dataRouter } from "../routes/dataRoute.js";
+
 import cors from "cors";
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/server", dataRouter);
+app.get("/", (req, res) => {
+  res.send("home");
+});
+
 app.listen(port, () => {
   console.log(`success http://localhost:${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      console.log(latitude);
+      console.log(longitude);
+    });
+  }, []);
   return <div>App</div>;
 };
 
